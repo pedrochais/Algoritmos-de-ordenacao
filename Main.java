55b9b6e7ca64761c9507466a1cce7277fa302f01
Pedro Rocha <pedrorocha.bou@gmail.com>
2022-05-11 17:49:59 -0300

    questao 2

--- Main.java
+++ Main.java
@@ -8,13 +8,13 @@ public class Main {
     public static void main(String[] args) {
         Random random = new Random();
 
-        Generic<String, Integer>[] vetor = new Generic[10000];
+        Generic<String, Integer>[] vetor = new Generic[20];
 
-        System.out.println("Vetor não ordenado");
-        for (int i = 0; i < 10000; i++){
-            vetor[i] = new Generic<>(GeradorString.getRandomString(3), random.nextInt(100));
-            System.out.println("Chave: "+vetor[i].getChave()+" / Valor: "+vetor[i].getValor());
-        }
+//        System.out.println("Vetor não ordenado");
+//        for (int i = 0; i < 20; i++){
+//            vetor[i] = new Generic<>(GeradorString.getRandomString(3), random.nextInt(100));
+//            System.out.println("Chave: "+vetor[i].getChave()+" / Valor: "+vetor[i].getValor());
+//        }
 
 //        ESTABILIDADE OK
 //        InsertionSort.insertionSort(vetor, vetor.length);
@@ -24,11 +24,11 @@ public class Main {
 //        }
 
 
-        SelectionSort.selectionSort(vetor);
-        System.out.println("Vetor ordenado: ");
-        for(int i=0; i < vetor.length; i++){
-            System.out.println("Chave: " + vetor[i].getChave() + " Valor: " + vetor[i].getValor());
-        }
+//        SelectionSort.selectionSort(vetor);
+//        System.out.println("Vetor ordenado: ");
+//        for(int i=0; i < vetor.length; i++){
+//            System.out.println("Chave: " + vetor[i].getChave() + " Valor: " + vetor[i].getValor());
+//        }
 
 //        QuickSort.quickSort(vetor, 0, 19); //FALTA IMPLEMENTAR
 //        System.out.println("Vetor ordenado: ");
--- Sort/SelectionSort.java
+++ Sort/SelectionSort.java
@@ -17,20 +17,36 @@ public class SelectionSort {
         return vetor;
     }
 
-    public static Generic<?,?>[] selectionSort2(Generic<?,?>[] v) {
-        for (int i = 0; i < v.length; i++) {
+    public static Generic<?,?>[] selectionSort2(Generic<?,?>[] vetor) {
+        int n= vetor.length;
+        for(int i = 0, j = n - 1; i < j; i++, j--){
+            Generic<?,?> min = vetor[i];
+            Generic<?,?> max = vetor[i];
+            int i_min = i, i_max = i;
+            for (int k = i; k <= j; k++) {
+                if (vetor[k].comparator(max) > 0) {
+                    max = vetor[k];
+                    i_max = k;
+                } else if (vetor[k].comparator(min) < 0) {
+                    min = vetor[k];
+                    i_min = k;
+                }
+            }
+            Generic<?,?> temp; //variavel temporaria
+            temp = vetor[i];
+            vetor[i] = vetor[i_min];
+            vetor[i_min] = temp;
 
-            int i_menor = i;
-            for (int j = i + 1; j < v.length; j++)
-                if (v[j].comparator(v[i_menor]) < 0)
-                    i_menor = j;
-
-            Generic<?,?> aux = v[i];
-            v[i] = v[i_menor];
-            v[i_menor] = aux;
-
+            if (vetor[i_min] == max) {
+                temp = vetor[j];
+                vetor[j] = vetor[i_min];
+                vetor[i_min] = temp;
+            }else {
+                temp = vetor[j];
+                vetor[j] = vetor[i_max];
+                vetor[i_max] = temp;
+            }
         }
-
-        return v;
+        return vetor;
     }
 }
