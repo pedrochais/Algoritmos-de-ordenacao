890339fbe824b1ba02342eb535440337edb918d4
Pedro Rocha <pedrorocha.bou@gmail.com>
2022-05-14 16:32:43 -0300

    automatização

--- Main.java
+++ Main.java
@@ -91,21 +91,21 @@ public class Main {
     }
 
     public static void gerarVetorDadoA(Generic<?,?>[] vetor, int n){
-        System.out.println("Gerando vetor...");
+        System.out.println("Gerando vetor...\n");
         for (int i = 0; i < n; i++) {
             vetor[i] = new Generic<>(GeradorString.getRandomString(10), random.nextDouble());
         }
     }
 
     public static void gerarVetorDadoB(Generic<?,?>[] vetor, int n){
-        System.out.println("Gerando vetor...");
+        System.out.println("Gerando vetor...\n");
         for (int i = 0; i < n; i++) {
             vetor[i] = new Generic<>(random.nextFloat(), GeradorString.getRandomString(10));
         }
     }
 
     public static void gerarVetorDadoC(Generic<?,?>[] vetor, int n){
-        System.out.println("Gerando vetor...");
+        System.out.println("Gerando vetor...\n");
         for (int i = 0; i < n; i++) {
             vetor[i] = new Generic<>(random.nextDouble(), random.nextInt(1000));
         }
@@ -163,16 +163,17 @@ public class Main {
         String dado = "";
 
         //Quantidade de elementos
-        int n = 1000;
+        int n = 100000;
 
         //Quantidade de experimentos
-        int exp = 100;
+        int exp = 2;
 
-        int opcaoDado = menuDados();
-
+        //Instância do vetor original e do vetor cópia
         Generic<?, ?>[] vetor = new Generic[n];
         Generic<?, ?>[] vetorCp = new Generic[n];
 
+        /* MENU PARA ESCOLHA DO TIPO DE DADO
+        int opcaoDado = menuDados();
         if(opcaoDado == 1){
             vetor = (Generic<String, Double>[])vetor;
             vetorCp = (Generic<String, Double>[])vetorCp;
@@ -186,10 +187,202 @@ public class Main {
             vetorCp = (Generic<Double, Integer>[])vetorCp;
             gerarVetorDadoC(vetor, n);
         }
-
         copiarVetor(vetor, vetorCp);
+        MENU PARA ESCOLHA DO TIPO DE DADO FIM */
 
-        //Menu de navegação
+        int tipoOrdenacao = 0;
+        long atribuicoes = 0;
+        long comparacoes = 0;
+        double tempoMedio = 0;
+        long tempoInicial = 0;
+        long tempoFinal = 0;
+        double somatorioTempo = 0;
+
+        /* EXECUÇÃO DE TODOS OS TESTES */
+        for (int opc = 0; opc < 3; opc++){
+            //Gerar vetores para os diferentes tipos de dados a cada iteração
+            if(opc == 0){
+                System.out.println("""
+                        
+                        ====================================================================
+                        EXECUÇÃO DOS ALGORITMOS DE ORDENAÇÃO | DADOS TIPO A (STRING, DOUBLE)
+                        ====================================================================
+                        """);
+                dado = "A (String, Double)";
+                vetor = (Generic<String, Double>[])vetor;
+                vetorCp = (Generic<String, Double>[])vetorCp;
+                gerarVetorDadoA(vetor, n);
+
+            }else if(opc == 1){
+                System.out.println("""
+                        
+                        ====================================================================
+                        EXECUÇÃO DOS ALGORITMOS DE ORDENAÇÃO | DADOS TIPO B (DOUBLE, STRING)
+                        ====================================================================
+                        """);
+                dado = "B (Double, String)";
+                vetor = (Generic<Double, String>[])vetor;
+                vetorCp = (Generic<Double, String>[])vetorCp;
+                gerarVetorDadoB(vetor, n);
+            }else if(opc == 2){
+                System.out.println("""
+                        
+                        ====================================================================
+                        EXECUÇÃO DOS ALGORITMOS DE ORDENAÇÃO | DADOS TIPO C (DOUBLE, INTEGER)
+                        ====================================================================
+                        """);
+                dado = "C (Double, Integer)";
+                vetor = (Generic<Double, Integer>[])vetor;
+                vetorCp = (Generic<Double, Integer>[])vetorCp;
+                gerarVetorDadoC(vetor, n);
+            }
+            copiarVetor(vetor, vetorCp);
+
+            /* QUICK SORT */
+
+            alg = "QuickSort";
+            tempoMedio = calcularTempoMedio(vetor, vetorCp, qs, exp, 1);
+            atribuicoes = qs.getAtr();
+            comparacoes = qs.getComp();
+            imprimirDados(dado, alg, "Crescente", n, exp, tempoMedio, atribuicoes, comparacoes);
+            copiarVetor(vetorCp, vetor);
+
+            alg = "QuickSort";
+            tempoMedio = calcularTempoMedio(vetor, vetorCp, qs, exp, 2);
+            atribuicoes = qs.getAtr();
+            comparacoes = qs.getComp();
+            imprimirDados(dado, alg, "Decrescente", n, exp, tempoMedio, atribuicoes, comparacoes);
+            copiarVetor(vetorCp, vetor);
+
+            /* HEAP SORT */
+
+            alg = "HeapSort";
+            tempoMedio = calcularTempoMedio(vetor, vetorCp, hs, exp, 1);
+            atribuicoes = hs.getAtr();
+            comparacoes = hs.getComp();
+            imprimirDados(dado, alg, "Crescente", n, exp, tempoMedio, atribuicoes, comparacoes);
+            copiarVetor(vetorCp, vetor);
+
+            alg = "HeapSort";
+            tempoMedio = calcularTempoMedio(vetor, vetorCp, hs, exp, 2);
+            atribuicoes = hs.getAtr();
+            comparacoes = hs.getComp();
+            imprimirDados(dado, alg, "Decrescente", n, exp, tempoMedio, atribuicoes, comparacoes);
+            copiarVetor(vetorCp, vetor);
+
+            /* MERGE SORT */
+
+            alg = "MergeSort";
+            tempoMedio = calcularTempoMedio(vetor, vetorCp, ms, exp, 1);
+            atribuicoes = ms.getAtr();
+            comparacoes = ms.getComp();
+            imprimirDados(dado, alg, "Crescente", n, exp, tempoMedio, atribuicoes, comparacoes);
+            copiarVetor(vetorCp, vetor);
+
+            alg = "MergeSort";
+            tempoMedio = calcularTempoMedio(vetor, vetorCp, ms, exp, 2);
+            atribuicoes = ms.getAtr();
+            comparacoes = ms.getComp();
+            imprimirDados(dado, alg, "Decrescente", n, exp, tempoMedio, atribuicoes, comparacoes);
+            copiarVetor(vetorCp, vetor);
+
+            /* MERGE SORT OTIMIZADO */
+
+            alg = "MergeSort Otimizado";
+            tempoMedio = calcularTempoMedio(vetor, vetorCp, mso, exp, 1);
+            atribuicoes = mso.getAtr();
+            comparacoes = mso.getComp();
+            imprimirDados(dado, alg, "Crescente", n, exp, tempoMedio, atribuicoes, comparacoes);
+            copiarVetor(vetorCp, vetor);
+
+            alg = "MergeSort Otimizado";
+            tempoMedio = calcularTempoMedio(vetor, vetorCp, mso, exp, 2);
+            atribuicoes = mso.getAtr();
+            comparacoes = mso.getComp();
+            imprimirDados(dado, alg, "Decrescente", n, exp, tempoMedio, atribuicoes, comparacoes);
+            copiarVetor(vetorCp, vetor);
+
+            /* JDK MERGESORT */
+            somatorioTempo = 0;
+            alg = "JDK Collections";
+            for (int i = 0; i < exp; i++){
+                aux = Arrays.asList(vetor);
+                tempoInicial = System.currentTimeMillis();
+                Collections.sort(aux);
+                tempoFinal = System.currentTimeMillis();
+
+                somatorioTempo += (tempoFinal - tempoInicial);
+
+                copiarVetor(vetorCp, vetor);
+            }
+
+            tempoMedio = somatorioTempo/exp;
+            imprimirDados(dado, alg, "Crescente", n, exp, tempoMedio, 0, 0);
+            copiarVetor(vetorCp, vetor);
+
+            /* JDK Array (LegacyMergeSort, Timsort) */
+            somatorioTempo = 0;
+            alg = "JDK Array";
+            for (int i = 0; i < exp; i++){
+                tempoInicial = System.currentTimeMillis();
+                Arrays.sort(vetor, cv);
+                tempoFinal = System.currentTimeMillis();
+
+                somatorioTempo += (tempoFinal - tempoInicial);
+
+                copiarVetor(vetorCp, vetor);
+            }
+
+            tempoMedio = somatorioTempo/exp;
+            imprimirDados(dado, alg, "Crescente", n, exp, tempoMedio, 0, 0);
+            copiarVetor(vetorCp, vetor);
+
+            /* SELECTION SORT OTIMIZADO */
+
+            /*
+            alg = "SelectionSort Otimizado";
+            tempoMedio = calcularTempoMedio(vetor, vetorCp, sso, exp, 1);
+            atribuicoes = sso.getAtr();
+            comparacoes = sso.getComp();
+            imprimirDados(dado, alg, "Crescente", n, exp, tempoMedio, atribuicoes, comparacoes);
+            copiarVetor(vetorCp, vetor);
+            */
+
+            /*
+            alg = "SelectionSort Otimizado";
+            tempoMedio = calcularTempoMedio(vetor, vetorCp, sso, exp, 2);
+            atribuicoes = sso.getAtr();
+            comparacoes = sso.getComp();
+            imprimirDados(dado, alg, "Decrescente", n, exp, tempoMedio, atribuicoes, comparacoes);
+            copiarVetor(vetorCp, vetor);
+            */
+
+            /* SELECTION SORT */
+
+            /*
+            alg = "SelectionSort";
+            tempoMedio = calcularTempoMedio(vetor, vetorCp, ss, exp, 1);
+            atribuicoes = ss.getAtr();
+            comparacoes = ss.getComp();
+            imprimirDados(dado, alg, "Crescente", n, exp, tempoMedio, atribuicoes, comparacoes);
+            copiarVetor(vetorCp, vetor);
+            */
+
+            /*
+            alg = "SelectionSort";
+            tempoMedio = calcularTempoMedio(vetor, vetorCp, ss, exp, 2);
+            atribuicoes = ss.getAtr();
+            comparacoes = ss.getComp();
+            imprimirDados(dado, alg, "Decrescente", n, exp, tempoMedio, atribuicoes, comparacoes);
+            copiarVetor(vetorCp, vetor);
+            */
+
+            System.out.println("[Iteração finalizada: Dado "+dado+"]");
+//            System.out.println("Pressione ENTER para continuar");
+//            input.nextLine();
+        }
+
+        /* MENU PRINCIPAL
         String opcao;
         while(true){
             opcao = menu();
@@ -276,7 +469,7 @@ public class Main {
                     comparacoes = mso.getComp();
                     break;
 
-                case "8": //JDKMergeSort
+                case "8": //JDK Collections
                     alg = "JDKMergeSort";
                     for (int i = 0; i < exp; i++){
                         aux = Arrays.asList(vetor);
@@ -293,7 +486,7 @@ public class Main {
 
                     break;
 
-                case "9": //JDKQuickSort
+                case "9": //JDK Arrays
                     alg = "JDKQuickSort";
                     for (int i = 0; i < exp; i++){
                         tempoInicial = System.currentTimeMillis();
@@ -329,5 +522,6 @@ public class Main {
             System.out.println("Atribuições: "+atribuicoes+" / Comparações: "+comparacoes);
             copiarVetor(vetorCp, vetor);
         }
+        MENU PRINCIPAL FIM */
     }
 }
